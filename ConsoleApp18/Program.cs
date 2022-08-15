9e7744aae217e4d9a300b3499e045d723084549b
Esdras Lima <esdraslimaf@gmail.com>
2022-08-15 13:17:43 -0300

    Adicionado endereço personalizado para pasta + melhorias

--- ConsoleApp18/Program.cs
+++ ConsoleApp18/Program.cs
@@ -9,69 +9,57 @@ namespace ConsoleApp18
     {
         static void Main(string[] args)
         {
-            string documentFolder = @"C:\temp\doc.csv";
-            string creatFolder = @"C:\temp\out";
-            string product;
-            double price;
-            int unity;
-
+            Console.WriteLine("The file must contain on each line: Product name, price, unit.");
+            Console.WriteLine("Ex: TV, 600.00, 2");
+            Console.WriteLine();
+            Console.Write(@"Enter file full path. Ex: C:\Temp\file.csv ");
+            Console.WriteLine();
+            string sourceFilePath = Console.ReadLine();
+        
             try
             {
-                bool existe = File.Exists(documentFolder);
-              
-                {
-                    if (existe == true)
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string pathToNewFolder = Path.GetDirectoryName(sourceFilePath);
+               
+                Directory.CreateDirectory(pathToNewFolder+@"\out");
+
+                string pathToCreateFile = pathToNewFolder + @"\out\summary.csv";
+
+                using (StreamWriter sw = File.AppendText(pathToCreateFile))
+                {                 
+                    for (int i = 0; i < lines.Length; i++)
                     {
-                        Directory.CreateDirectory(creatFolder);
+
+                        string separatelines = lines[i];
+
+                        string[] separetelinesarray = separatelines.Split(',');
+
+                        //Product's name
+                        string product = separetelinesarray[0];
+                        //Product's price
+                        double price = Convert.ToDouble(separetelinesarray[1], CultureInfo.InvariantCulture);
+                        //Product's unity
+                        int unity = Convert.ToInt32((separetelinesarray[2]));
+
+                        double totalPrice = price * unity;
+
+                        string text = product + " " + totalPrice.ToString("F2", CultureInfo.InvariantCulture);
+                        sw.WriteLine(text);
                     }
-
-                    string[] lines = File.ReadAllLines(documentFolder);
-
-                    using (StreamWriter sw = File.AppendText(@"C:\temp\out\summary.csv"))
-                    {
-
-                        Console.WriteLine("O documento se encontra na pasta: " + documentFolder);
-                        Directory.CreateDirectory(creatFolder);
-                        Console.WriteLine("Pasta 'out' criada em: " + creatFolder);
-
-
-                        Console.WriteLine(creatFolder+@"\summary.csv foi criado");
-                        for (int i = 0; i < lines.Length; i++)
-                        {
-
-                            string separatelines = lines[i];
-
-                            string[] separetelinesarray = separatelines.Split(',');
-
-                            //Product's name
-                            product = separetelinesarray[0];
-                            //Product's price
-                            price = Convert.ToDouble(separetelinesarray[1], CultureInfo.InvariantCulture);
-                            //Product's unity
-                            unity = Convert.ToInt32((separetelinesarray[2]));
-
-                            double totalPrice = price * unity;
-
-                            string text = product + " " + totalPrice.ToString("F2", CultureInfo.InvariantCulture);
-                            sw.WriteLine(text);
-                        }
-                    }
-
-
+                    Console.WriteLine();
+                    Console.WriteLine("Successful");
                 }
-                
-            }
 
+
+            }
             catch (IOException e)
             {
-                Console.WriteLine("Ocorreu um erro: ");
+                Console.WriteLine();
+                Console.WriteLine("Failed!! An error was ocurred: ");
                 Console.WriteLine(e.Message);
             }
-
-
         }
-
     }
 }
-
 
---
+++ README.md
@@ -0,0 +1,2 @@
+# Manipulando-Arquivo
+![atividadeee](https://user-images.githubusercontent.com/101669187/184558043-66f4484b-f1bf-45e5-8aaf-86dccba57f81.png)
